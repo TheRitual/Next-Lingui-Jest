98b3214cf85c936adaa8a9cc62205795df6488e1
Marcin Kawczyński <dev@theRitual.eu>
2022-04-24 18:09:27 +0200

    Created project

---
+++ pages/test.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { CustomCard } from "../shared/presentation-components";
+
+const Home: NextPage = () => {
+  return <CustomCard>Element</CustomCard>;
+};
+
+export default Home;
---
+++ shared/presentation-components/index.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+import { Card } from "@mui/material";
+
+export const CustomCard = styled(Card)`
+  margin: 50px auto;
+  width: 300px;
+  height: 300px;
+  display: flex;
+  padding: 30px;
+  justify-content: center;
+  align-items: center;
+`;
